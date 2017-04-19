817191a4e52fe4415f078b0c7380531c956836c6
인상학 <platinasnow@naver.com>
2017-04-19 16:41:35 +0900

    log4net을 이용하여 로그 파일 생성 404, 500 에러 분기 처리

--- bmw_fs/Global.asax.cs
+++ bmw_fs/Global.asax.cs
@@ -15,6 +15,7 @@ namespace bmw_fs
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            log4net.Config.XmlConfigurator.Configure();
         }
 
         protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
@@ -30,5 +31,30 @@ namespace bmw_fs
                 }
             }
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            log4net.ILog logger = log4net.LogManager.GetLogger(typeof(MvcApplication));
+
+            var lastError = Server.GetLastError();
+            Exception exception = lastError;
+            var serverError = lastError as HttpException;
+
+            if (serverError != null && serverError.GetHttpCode() == 404)
+            {
+                Response.Redirect("/Error/Error404");
+            }
+            else if (serverError != null && serverError.GetHttpCode() == 403)
+            {
+                Response.Redirect("/Error/Error403");
+            }
+            else
+            {
+                logger.Error(exception.ToString());
+                Server.ClearError();
+                Response.Redirect("/Error/Error500");
+            }
+        }
     }
+
 }
