26be02e186b0e83faffc5c086d77eb0bb2abafb3
mj.son <mj.son@theuber.co.kr>
2017-04-25 15:18:03 +0900

    개인정보처리방침 insert 추가

---
+++ bmw_fs/Controllers/PrivacyController.cs
@@ -0,0 +1,35 @@
+﻿using bmw_fs.Models.privacy;
+using bmw_fs.Service.face.privacy;
+using bmw_fs.Service.impl.privacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bmw_fs.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        PrivacyService privacyService = new PrivacyServiceImpl();
+
+        public ActionResult list()
+        {
+            return View();
+        }
+
+        public ActionResult register()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateInput(false)]
+        public RedirectToRouteResult registerProc(Privacy privacy)
+        {
+            privacyService.insertPrivacy(privacy);
+
+            return RedirectToAction("list");
+        }
+    }
+}
---
+++ bmw_fs/Dao/face/privacy/PrivacyDao.cs
@@ -0,0 +1,17 @@
+﻿using bmw_fs.Models.privacy;
+using IBatisNet.DataMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bmw_fs.Dao.face.privacy
+{
+    public class PrivacyDao
+    {
+        public void insertPrivacy(Privacy privacy)
+        {
+            Mapper.Instance().Insert("privacy.insertPrivacy", privacy);
+        }
+    }
+}
