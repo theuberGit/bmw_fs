4c07fd19dbc9f238cd0e28e7922c416375b0d38a
mj.son <mj.son@theuber.co.kr>
2017-05-10 15:47:13 +0900

    Faq 파일 추가

---
+++ bmw_fs/Controllers/CustomerService/FaqController.cs
@@ -0,0 +1,38 @@
+﻿using bmw_fs.Models.CustomerService;
+using bmw_fs.Service.face.CustomerService;
+using bmw_fs.Service.impl.CustomerService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bmw_fs.Controllers.CustomerService
+{
+    [Authorize]
+    public class FaqController : Controller
+    {
+        FaqService faqService = new FaqServiceImpl();
+
+        public ActionResult list(Faq faq)
+        {
+
+            return View();
+        }
+
+        public ActionResult register(Faq faq)
+        {
+            return View();
+        }
+
+        public ActionResult view(Faq faq)
+        {
+            return View();
+        }
+
+        public ActionResult modify(Faq faq)
+        {
+            return View();
+        }
+    }
+}
