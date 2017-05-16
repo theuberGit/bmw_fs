6da55da86eab6d7cb11f5881053d47e30cefcdd3
인상학 <sh.in@theuber.co.kr>
2017-05-16 13:42:16 +0900

    메인롤링 일부 추가

---
+++ bmw_fs/Service/face/rolling/RollingService.cs
@@ -0,0 +1,25 @@
+﻿using bmw_fs.Models.rollling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace bmw_fs.Service.face.rolling
+{
+    interface RollingService
+    {
+        void insertRolling(HttpFileCollectionBase multipartFiles, Rolling rolling);
+
+        IList<Rolling> findAll(Rolling rolling);
+
+        int findAllCount(Rolling rolling);
+
+        Rolling findRolling(Rolling rolling);
+
+        void updateRollingt(HttpFileCollectionBase multipartFiles, Rolling rolling);
+
+        void deleteRolling(Rolling rolling);
+    }
+}
---
+++ bmw_fs/Service/impl/rolling/RollingServiceImpl.cs
@@ -0,0 +1,79 @@
+﻿using bmw_fs.Service.face.rolling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using bmw_fs.Models.rollling;
+using IBatisNet.DataMapper;
+using bmw_fs.Dao.face.rolling;
+using bmw_fs.Service.face.common;
+using bmw_fs.Service.impl.common;
+using bmw_fs.Common;
+using Microsoft.Ajax.Utilities;
+
+namespace bmw_fs.Service.impl.rolling
+{
+    public class RollingServiceImpl : RollingService
+    {
+        RollingDao rollingDao = new RollingDao();
+        FilesService filesService = new FilesServiceImpl();
+        SequenceService sequenceService = new SequenceServiceImpl();
+
+        public void deleteRolling(Rolling rolling)
+        {
+            Mapper.Instance().BeginTransaction();
+            this.rollingDao.deleteRolling(rolling);
+            filesService.deleteRealFilesAndDataByFileMasterIdx(rolling.idx);
+            Mapper.Instance().CommitTransaction();
+        }
+
+        public IList<Rolling> findAll(Rolling rolling)
+        {
+            return rollingDao.findAll(rolling);
+        }
+
+        public int findAllCount(Rolling rolling)
+        {
+            return rollingDao.findAllCount(rolling);
+        }
+
+        public Rolling findRolling(Rolling rolling)
+        {
+            return rollingDao.findRolling(rolling);
+        }
+
+        public void insertRolling(HttpFileCollectionBase multipartFiles, Rolling rolling)
+        {
+            int masterIdx = sequenceService.getSequenceMasterIdx();
+            rolling.idx = masterIdx;
+            Mapper.Instance().BeginTransaction();
+            validation(rolling);
+           this.rollingDao.insertRolling(rolling);
+            filesService.fileUpload(multipartFiles, "KRImg", "jpg|png", 5 * 1024 * 1024, masterIdx, null);
+            filesService.fileUpload(multipartFiles, "ERImg", "jpg|png", 5 * 1024 * 1024, masterIdx, null);
+            Mapper.Instance().CommitTransaction();
+        }
+
+        public void updateRollingt(HttpFileCollectionBase multipartFiles, Rolling rolling)
+        {
+            Mapper.Instance().BeginTransaction();
+            filesService.deleteFileAndFileUpload(multipartFiles, "KRImg", "jpg|png", 5 * 1024 * 1024, rolling.idx, rolling.rollingIdx);
+            validation(rolling);
+            this.rollingDao.updateRolling(rolling);
+            Mapper.Instance().CommitTransaction();
+        }
+
+        public void validation(Rolling rolling)
+        {
+            if (String.IsNullOrWhiteSpace(rolling.title)) throw new CustomException("필수 값이 없습니다.(제목)");
+            if (String.IsNullOrWhiteSpace(rolling.engSite)) throw new CustomException("필수 값이 없습니다.(영문 사이트)");
+            if (String.IsNullOrWhiteSpace(rolling.deployYn)) throw new CustomException("필수 값이 없습니다.(배포)");
+            if (String.IsNullOrWhiteSpace(rolling.gubun)) throw new CustomException("필수 값이 없습니다.(구분)");
+            if (String.IsNullOrWhiteSpace(rolling.korButtonName)) throw new CustomException("필수 값이 없습니다.(버튼명(국문))");
+            if (String.IsNullOrWhiteSpace(rolling.KorButtonUrl)) throw new CustomException("필수 값이 없습니다.(버튼URL(국문))");
+            if (String.IsNullOrWhiteSpace(rolling.engButtonName)) throw new CustomException("필수 값이 없습니다.(버튼명(영문))");
+            if (String.IsNullOrWhiteSpace(rolling.engButtonUrl)) throw new CustomException("필수 값이 없습니다.(버튼URL(영문))");
+            //int형식 벨리데이션 처리? (order)
+        }
+    }
+}
