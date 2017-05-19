911d0559c8a573dff818c5cab95a593c8554bcd6
인상학 <sh.in@theuber.co.kr>
2017-05-19 23:17:35 +0900

    관리자 관리 페이지 추가 1:1 문의 밑작업

---
+++ bmw_fs/Controllers/admin/AdminController.cs
@@ -0,0 +1,81 @@
+﻿using bmw_fs.Models.admin;
+using bmw_fs.Service.face.admin;
+using bmw_fs.Service.face.common;
+using bmw_fs.Service.impl.admin;
+using bmw_fs.Service.impl.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace bmw_fs.Controllers.admin
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        SearchService searchService = new SearchServiceImpl();
+        MemberService memberService = new MemberServiceImpl();
+
+        public ActionResult list(Member member)
+        {
+            searchService.setSearchSession(Request, Session);
+            searchService.setPagination(member, 10, memberService.findAllCount(member));
+            ViewBag.list = memberService.findAll(member);
+            ViewBag.pagination = member;
+            ViewBag.today = DateTime.Now;
+
+            return View();
+        }
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public RedirectToRouteResult registerProc(Member member)
+        {
+            HttpFileCollectionBase multipartRequest = Request.Files;
+            member.regId = System.Web.HttpContext.Current.User.Identity.Name;
+            memberService.insertMember(member);
+
+
+            return RedirectToAction("list", (RouteValueDictionary)Session["searchMap"]);
+        }
+
+        public ActionResult view(Member member)
+        {
+            Member item = memberService.findMemberByIdx(member);
+            ViewBag.item = item;
+            return View();
+        }
+
+        public ActionResult modify(Member member)
+        {
+            Member item = memberService.findMemberByIdx(member);
+            ViewBag.item = item;
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public RedirectToRouteResult modifyProc(Member member)
+        {
+            HttpFileCollectionBase multipartRequest = Request.Files;
+            member.uptId = System.Web.HttpContext.Current.User.Identity.Name;
+            memberService.updateMember(member);
+            return RedirectToAction("list");
+        }
+
+        [HttpPost]
+        public RedirectToRouteResult delete(Member member)
+        {
+            memberService.deleteMember(member);
+            return RedirectToAction("list");
+        }
+
+    }
+}
