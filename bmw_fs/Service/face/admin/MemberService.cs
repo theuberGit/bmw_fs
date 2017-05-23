c9334c9675db219999109ee3f79449261b18e901
인상학 <sh.in@theuber.co.kr>
2017-05-23 17:59:00 +0900

    관리자 관리 수정, 비밀번호 변경, validation 기능 추가

--- bmw_fs/Service/face/admin/MemberService.cs
+++ bmw_fs/Service/face/admin/MemberService.cs
@@ -24,6 +24,9 @@ namespace bmw_fs.Service.face.admin
         void deleteMember(Member member);
 
         void updateLoginDate(Member member);
-        
+
+        bool findMemberDuplicated(Member member);
+
+
     }
 }
